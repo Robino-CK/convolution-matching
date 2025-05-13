dd6a22adf470510af124a7bee79d40f9cb261f3b
Robino-CK <robin.kollmann@yahoo.de>
2025-05-13 11:23:48 +0200

    moved test to own file

--- GraphSummarizers/Coarsener/HeteroCoarsener.py
+++ GraphSummarizers/Coarsener/HeteroCoarsener.py
@@ -6,11 +6,12 @@ import sys
 import math
 import time
 import torch
+
 from sklearn.decomposition import PCA
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(THIS_DIR, "../../"))
 from Datasets.Dataset import Dataset
-
+from test_hetero_coarsening import TestHomo
 from sklearn.preprocessing import MinMaxScaler
 from GraphSummarizers.GraphSummarizer import GraphSummarizer
 from GraphSummarizers.GraphSummarizer import SUMMARY_GRAPH_FILENAME
@@ -772,178 +773,11 @@ class HeteroCoarsener(GraphSummarizer):
         
 
 
-class Tester(): 
-    def __init__(self):
-        self.s = dict()
-        self.s[0] = (2 / np.sqrt(2))
-        self.s[1] = (1 / np.sqrt(2)) * (3 + 5) + (1 / np.sqrt(3)) * (4)
-        self.s[2] = (1 / np.sqrt(3)) * (4) 
-        self.s[3] = 0
-        self.s[4] = 0
-        
-        self.h = dict()
-        self.h[0] = 1.0
-        self.h[1] = (1 / np.sqrt(4)) * self.s[1]
-        self.h[2] = (1 / np.sqrt(2)) * self.s[2]
-        self.h[3] = 0
-        self.h[4] = 0
-    
-        self.nearest_neighbors = {0: {1,2}, 1:{0,2}, 2:{0,1}, 3:{2,4}, 4:{3}}
-    def create_test_graph(self):
-        g = dgl.heterograph({
-            ('user', 'follows', 'user'): ([0, 1, 1, 1, 2], [1, 2, 3, 4,3])})
-        g.nodes['user'].data['feat'] = torch.tensor([[1.0],[2.0],[3.0],[4.0],[5.0]])
-        
-
-        return g
-
-
-
-    def check_H(self):
-        for k, v in self.s.items():
-            assert math.isclose(self.coarsener.coarsened_graph.nodes["user"].data[f'sfollows'][k].item(), v, rel_tol=1e-6), f"error in creating H for {k}"
-            
-        for k, v in self.h.items():
-            assert math.isclose(self.coarsener.coarsened_graph.nodes["user"].data[f'hfollows'][k].item(), v, rel_tol=1e-6), f"error in creating H for {k}"
-        
-    def check_init_H_costs(self):
-        neighbors = self.coarsener.init_neighbors["user"]
-        correct = self.nearest_neighbors
-        assert neighbors == correct, f"error in init neighbors {neighbors} != {correct}"
-    
-    def check_init_feat_costs(self):
-        
-        costs = self.coarsener.init_costs_dict_features["user"]["costs"]
-        index = self.coarsener.init_costs_dict_features["user"]["index"]
-        self.correct_feat_costs = {(0,1): 1, (1,0): 1 ,(2,0):2,   (0,2): 2, (1,2): 1, (2,1):1, (3,2): 1, (4,3) : 1, (3,4): 1}
-        for i in range(costs.shape[0]):
-            node1 = index[0][i].item()
-            node2 = index[1][i].item()
-            cost = costs[i].item()
-            if (node1, node2) in self.correct_feat_costs:
-                assert math.isclose(cost, self.correct_feat_costs[(node1, node2)], rel_tol=1e-6), f"error in init feat costs {cost} != {self.correct_feat_costs[(node1, node2)]}"
-            else:
-                assert math.isclose(cost, 0.0, rel_tol=1e-6), f"error in init feat costs {cost} != 0"
-            assert len(self.correct_feat_costs) == costs.shape[0], f"error in init feat costs {costs.shape[0]} != {len(self.correct_feat_costs)}"
-            
-    def check_init_H_costs(self):
-        
-        self.correct_H_costs = { }
-        for k, list_values in self.nearest_neighbors.items():
-            for v in list_values:
-                
-                huv = (self.s[k] + self.s[v]) / np.sqrt(self.g.out_degrees(k) + self.g.out_degrees(v) + 2)
-                self.correct_H_costs[k,v] =  torch.norm(torch.tensor(huv - self.h[k]),  p=1).item() + torch.norm(torch.tensor(huv -self.h[v]),  p=1).item()
-        costs = self.coarsener.init_costs_dict_etype["user"]["follows"]["costs"]
-        index = self.coarsener.init_costs_dict_etype["user"]["follows"]["index"]
-        for i in range(costs.shape[0]):
-            node1 = index[0][i].item()
-            node2 = index[1][i].item()
-            cost = costs[i].item()
-            if (node1, node2) in self.correct_H_costs:
-                assert math.isclose(cost, self.correct_H_costs[(node1, node2)], rel_tol=1e-2), f"error in init feat costs {cost} != {self.correct_H_costs[(node1, node2)]}"
-            else:
-                assert math.isclose(cost, 0.0, rel_tol=1e-6), f"error in init feat costs {cost} != 0"
-        assert len(self.correct_H_costs) == costs.shape[0], f"error in init feat costs {costs.shape[0]} != {len(self.correct_H_costs)}"
-
-    def check_init_total_costs(self):
-        costs = self.coarsener.init_costs_dict["user"]
-        index = self.coarsener.init_index_dict["user"]
-        correct_costs = dict()
-        for k, list_values in self.nearest_neighbors.items():
-            for v in list_values:
-                correct_costs[k,v] = (self.correct_H_costs[k,v] / 2.9831)  + (self.correct_feat_costs[k,v] / 1)
-                
-        for i in range(costs.shape[0]):
-            node1 = index[0][i].item()
-            node2 = index[1][i].item()
-            cost = costs[i].item()
-            if (node1, node2) in self.correct_H_costs:
-                assert math.isclose(cost, correct_costs[(node1, node2)], rel_tol=1e-2), f"error in init feat costs {cost} != {correct_costs[(node1, node2)]}"
-            else:
-                assert math.isclose(cost, 0.0, rel_tol=1e-6), f"error in init feat costs {cost} != 0"
-        assert len(self.correct_H_costs) == costs.shape[0], f"error in init feat costs {costs.shape[0]} != {len(self.correct_H_costs)}"
-
-    def check_first_merge_candidates(self):
-        candidates = [(4,3), (1,2)]
-        assert self.coarsener.candidates["user"] == candidates, "error first merge candidates"
-        
-    def check_first_merge_nodes(self):
-        assert all(self.coarsener.coarsened_graph.edges()[0] ==  torch.tensor([0,2])), "error merged edges not correct"
-        assert all(self.coarsener.coarsened_graph.edges()[1] ==  torch.tensor([2,1]) ), "error merged edges not correct"
-        
-        assert all(self.coarsener.coarsened_graph.nodes["user"].data["feat"] == torch.tensor([[1.0], [4.5], [2.5]])), "error merge features not correct"
-        
-    def check_first_merge_s_and_h(self):
-        correct_s ={
-            0: self.s[0],
-                2: (self.s[1] + self.s[2]),
-            1: (self.s[3] + self.s[4]),
-        }
-        
-        for k, v in correct_s.items():
-            assert math.isclose(self.coarsener.coarsened_graph.nodes["user"].data[f'sfollows'][k].item(), v, rel_tol=1e-6), f"error in updating S for {k}"       
-        
-        correct_h = {
-            0: correct_s[0] / np.sqrt(2),
-            1: correct_s[1] / np.sqrt(2 ),
-            2: correct_s[2] / np.sqrt(2 + 2),
-        }
-        for k, v in correct_h.items():
-            assert math.isclose(self.coarsener.coarsened_graph.nodes["user"].data[f'hfollows'][k].item(), v, rel_tol=1e-6), f"error in updating H for {k}"
-        
-        pass
-    
-    def check_first_merge_features(self):
-        correct_feat = {
-            0: torch.tensor([1.0]),
-            1: torch.tensor([4.5]),
-            2: torch.tensor([2.5]),
-        }
-        for k, v in correct_feat.items():
-            assert math.isclose(self.coarsener.coarsened_graph.nodes["user"].data["feat"][k].item(), v.item(), rel_tol=1e-6), f"error in updating features for {k}"
-    
-    def check_first_merge_updated_merge_edges(self):
-        recalc_edges = (torch.tensor([0,1,2]), torch.tensor([2,2,0]))
-        assert all(self.coarsener.merge_graphs["user"].find_edges(self.coarsener.edge_ids_need_recalc)[0] == recalc_edges[0]), "error in updated merge edges"
-        assert all(self.coarsener.merge_graphs["user"].find_edges(self.coarsener.edge_ids_need_recalc)[1] == recalc_edges[1]), "error in updated merge edges"
-        
-        new_feature_costs = torch.tensor([1.5, 2, 1.5])
-        assert  all(self.coarsener.costs_features == new_feature_costs), "error in updated merge edges"
-        
-        new_h_costs = torch.tensor([
-            abs((self.s[0] + self.s[1] + self.s[2] )/ np.sqrt(5) - (self.s[0] / np.sqrt(2))) +  abs((self.s[0] + self.s[1] + self.s[2]) / np.sqrt(5) - ((self.s[1] + self.s[2]) / np.sqrt(4))),
-            abs((self.s[1] + self.s[2] + self.s[3] + self.s[4] )/ np.sqrt(6) -  (self.s[1] + self.s[2]) / np.sqrt(4)) +  abs((self.s[1] + self.s[2] + self.s[3] + self.s[4]) / np.sqrt(6) - (self.s[3] + self.s[4] / np.sqrt(4))),
-            abs((self.s[0] + self.s[1] + self.s[2] )/ np.sqrt(5) - self.s[0] / np.sqrt(2)) +  abs((self.s[0] + self.s[1] + self.s[2]) / np.sqrt(5) - ((self.s[1] + self.s[2]) / np.sqrt(4))),
-        ])
-        new_h_costs = new_h_costs / 2.9831
-        for i in range(new_h_costs.shape[0]):
-            assert math.isclose(new_h_costs[i].item(), self.coarsener.costs_H[i].item(), rel_tol=1e-2), f"error in updated merge edges {new_h_costs[i]} != {self.coarsener.costs_H[i]}"
-        
-        
-    def run_test(self):
-        self.g = self.create_test_graph()
-        self.coarsener = HeteroCoarsener(None, self.g, 0.5, num_nearest_per_etype=2, num_nearest_neighbors=2,pairs_per_level=30)
-        self.coarsener.init_step()
-        self.check_H()
-        self.check_init_H_costs()
-        self.check_init_feat_costs()
-        self.check_init_total_costs()
-        self.check_first_merge_candidates()
-        self.coarsener.iteration_step()
-        self.check_first_merge_nodes()
-        self.check_first_merge_s_and_h()
-        self.check_first_merge_features()
-        self.check_first_merge_updated_merge_edges()
-        
-        
-    
-    
-
 
 if __name__ == "__main__":
-    tester = Tester()
-    tester.run_test()
+    tester = TestHomo()
+    g = tester.g 
+    tester.run_test(HeteroCoarsener(None,g, 0.5, num_nearest_per_etype=2, num_nearest_neighbors=2,pairs_per_level=30))
     dataset = DBLP() 
     original_graph = dataset.load_graph()
 
